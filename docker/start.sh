c674a4a981e263782fe43e5382fdb2c6bf4aeb8b
I Made Brahmanda Setyadi <brahmandasetyadi00@gmail.com>
2026-03-06 15:34:23 +0800

    feat: Add Docker Nginx configuration and an application startup script.

--- docker/start.sh
+++ docker/start.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 
 # Menunggu database siap (opsional, tapi disarankan)
 echo "Waiting for database connection..."
