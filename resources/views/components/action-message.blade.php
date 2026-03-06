2ec764f5258350c219f5a2aeddd850ea0fbd16d1
I Made Brahmanda Setyadi <brahmandasetyadi00@gmail.com>
2026-03-06 23:58:37 +0800

    Update Appservice

--- resources/views/components/action-message.blade.php
+++ resources/views/components/action-message.blade.php
@@ -1,5 +1,4 @@
 @props(['on'])
-
 <div x-data="{ shown: false, timeout: null }"
      x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000); })"
      x-show.transition.out.opacity.duration.1500ms="shown"
